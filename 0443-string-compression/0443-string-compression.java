5685806eeb813220325b9950ac02029f91e3edfe
shrinidhi <shrinidhisherugar2024@gmail.com>
2026-08-06 22:48:57 +0530

    Time: 2 ms (27.63%), Space: 45.4 MB (66.13%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int compress(char[] chars) {
+
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+
+        for (int i = 0; i < chars.length; i++) {
+
+            if (i == chars.length - 1 || chars[i] != chars[i + 1]) {
+                sb.append(chars[i]);
+
+                if (count > 1)
+                    sb.append(count);
+
+                count = 1;
+            } else {
+                count++;
+            }
+        }
+
+        for (int i = 0; i < sb.length(); i++)
+            chars[i] = sb.charAt(i);
+
+        return sb.length();
+    }
+}
